1ab1be9f55d01a20e84ec28046b608af89a86dc3
Jesse Galloway <jsg220@lehigh.edu>
2021-01-20 17:01:05 -0500

    predictor research

--- night_trader/data_sourcer.py
+++ night_trader/data_sourcer.py
@@ -1,22 +1,60 @@
+import csv
+import pandas as pd
+import threading
+
 
 class DataSourcer:
 
-    CRYPTO = 'ETH'
+    CRYPTO = 'ETH' #ETH, BTC, or LTC?
 
     eth_history = {}
+    quotes = pd.DataFrame(columns=['time', 'price'])
+    last_get_index = 0
 
     def __init__(self, r):
-        print("Gathering historical data...", end='')
-        self.eth_history = r.crypto.get_crypto_historicals(self.CRYPTO, interval='5minute', span='week', bounds='24_7', info=None) #TODO change to 15second
-        if not self.eth_history:
-            print("Error in loading history data, system exiting.")
-            exit()
-        else:
-            print("done")
-        return
+        # self.login = r
+        # self.eth_history = self.getHistory(self.CRYPTO, r)
+        # if not self.eth_history:
+        #     print("no history, exiting")
+        #     exit()
+        
+        # with open('test.csv', 'w', encoding='utf8', newline='') as output_file:
+        #     fc = csv.DictWriter(output_file, fieldnames=self.eth_history[0].keys())
+        #     fc.writeheader()
+        #     fc.writerows(self.eth_history)
+        # return
+        pass
+
+    # def getHistory(self, crypto_symbol, r) -> list:
+    #     print("Gathering historical data...", end='')
+    #     history_list = r.crypto.get_crypto_historicals(crypto_symbol, interval='5minute', span='week', bounds='24_7', info=None) #TODO change to 15second
+
+    #     if not history_list:
+    #         print("Error in loading history data, system exiting.")
+    #         return []
+    #     else:
+    #         print("done")
+    #         return history_list
+
+    def pullNewPrice(self, r):
+        new_data_point = {'time': pd.Timestamp.now(), 'price':float(r.crypto.get_crypto_quote(self.CRYPTO, info='mark_price'))}
+        self.quotes = self.quotes.append(new_data_point, ignore_index=True)
+        print("ETH:", new_data_point['price'])
+
+    def getNewQuotes(self) -> pd.DataFrame:
+        to_return = self.quotes[self.last_get_index:]
+        self.last_get_index = len(self.quotes)
+        return to_return
+
+    def isNewQuotes(self) -> bool:
+        return len(self.quotes) > self.last_get_index
+
+    def run(self, r):
+        self.pullNewPrice(r)
+        threading.Timer(1.0, self.run, [r]).start()
     
-    def getCurrentQuote(self, r):
-        print(r.crypto.get_crypto_quote(self.CRYPTO, info=None))
-        print("waiting now")
+    def justGetMostRecentPrice(self) -> float:
+        return self.quotes['price'].iloc[-1]
+        
 
 
--- night_trader/runner.py
+++ night_trader/runner.py
@@ -1,10 +1,16 @@
 import robin_stocks as r
+import time
+import threading
 from data_sourcer import DataSourcer
+from predictors.wma import Wma
 
 class NightTrader():
 
     login: dict
-    data_source: DataSourcer
+    predictor = None
+    dataSourcer: DataSourcer
+    bought = (False, 0.0)
+    sumwin = 0
 
     def __init__(self):
         print("---------------------------------- Night Trader ----------------------------------")
@@ -23,15 +29,39 @@ class NightTrader():
             exit()
         print("done")
 
-        # Create data source object which pulls history on init
-        self.data_source = DataSourcer(r)
-        self.data_source.getCurrentQuote(r)
+
+        self.dataSourcer = DataSourcer(r)
+        self.dataSourcer.run(r)
+
+        self.predictor = Wma(self.dataSourcer)        
 
     def logout(self):
         # print(r.crypto.get_crypto_positions())
         # log out of robinhood at the end of the session
         r.authentication.logout()
+    
+    def run(self):
+        # self.data_source.printPriceHistory()
+        # if len(self.data_source.getData()) > 15:
+        slope = self.predictor.predict()
+        current_price = self.dataSourcer.justGetMostRecentPrice()
+        if slope > 0.005 and not self.bought[0]:
+            self.bought = (True, current_price)
+            print("bought at", current_price)
+
+        # sell if going down or have lost $10 already
+        if (slope < -0.001 and self.bought[0]) or (self.bought[1] - current_price > 10):
+            profit = float(current_price) - float(self.bought[1])
+            print("Selling at",current_price, " for Profit: ", profit)
+            self.sumwin = self.sumwin + current_price - self.bought[1]
+            self.bought = (False, 0)
+            print("Total profit:", self.sumwin)
+
 
 if __name__ == "__main__":
     # execute only if run as a script
     nt = NightTrader()
+    while(True):
+        nt.run()
+        time.sleep(5)
+    nt.logout()
