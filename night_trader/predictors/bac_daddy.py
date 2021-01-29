185dc6162d0b72a7504008894a744c0be05fe491
Jesse Galloway <jsg220@lehigh.edu>
2021-01-29 08:13:12 -0500

    improved logging

--- night_trader/predictors/bac_daddy.py
+++ night_trader/predictors/bac_daddy.py
@@ -1,8 +1,5 @@
-import numpy as np
-from data_sourcer import DataSourcer
-from data_manager import DataManager
 import pandas as pd
-import time
+from datetime import datetime
 from predictors.lstm.lstm_data_manager import LstmDataManager
 
 
@@ -20,7 +17,7 @@ class BacDaddy:
 
         # Calculate MACD and signal lines
         bacd_params = (12, 26, 9)
-        bacd_multiplier = 111
+        bacd_multiplier = 25 #25 or 111
         exp1 = prices.ewm(span=bacd_params[0]*bacd_multiplier, adjust=False).mean()
         exp2 = prices.ewm(span=bacd_params[1]*bacd_multiplier, adjust=False).mean()
         macd = exp1 - exp2
@@ -28,10 +25,10 @@ class BacDaddy:
             
         # using the opposite of conventional logic...
         if (macd.iat[-1, 0] < signal.iat[-1, 0]) and (macd.iat[-2, 0] >= signal.iat[-2, 0]):
-            print("Got buying signal...")
+            print(f"[{datetime.now().strftime('%H:%M:%S')}] Got buying signal at ${latest_price:.2f}")
             return "buy"
         elif (macd.iat[-1, 0] > signal.iat[-1, 0]) and (macd.iat[-2, 0] <= signal.iat[-2, 0]):
-            print("Got selling signal...")
+            print(f"[{datetime.now().strftime('%H:%M:%S')}] Got Selling signal at ${latest_price:.2f}")
             return "sell"
         else:
             return None
