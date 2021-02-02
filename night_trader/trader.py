859175354d594c12342489a37afda0eeb36e8e40
jsngalloway <galloway.jesse@aurora.aero>
2021-02-02 09:57:12 -0500

    reworked trader for efficency

--- night_trader/trader.py
+++ night_trader/trader.py
@@ -1,91 +1,73 @@
 import time
+from typing import Tuple
 import numpy as np
 from datetime import datetime
+import robin_stocks as r
+import logging
+from trade import Trade
 
+# A transaction can be:
+# filled
+# cancelled
+# open
+# partially-filled?
 
-def buyAndWait(r):
-    price_info = r.crypto.get_crypto_quote("ETH", info=None)
-    # will_pay = round((float(price_info["ask_price"]) + float(price_info["mark_price"])) / 2, 2)
-    will_pay = round(float(price_info["ask_price"]), 2)
-    buy_info = r.orders.order_buy_crypto_limit("ETH", 0.05, will_pay)
-    print(
-        f"[{datetime.now().strftime('%H:%M:%S')}] Buying (Limit: ${round(will_pay, 2)})...",
-        end="",
-    )
-    cleared = False
-    id = buy_info["id"]
-    tries = 0
-    while not cleared:
-        order_info = r.orders.get_crypto_order_info(id)
-        if order_info["state"] == "filled":
-            print(
-                f"bought [{datetime.now().strftime('%H:%M:%S')}] (Exec: ${round(float(order_info['average_price']), 2)})"
-            )
-            cleared = True
-            return (True, float(order_info["average_price"]))
+log = logging.getLogger(__name__)
+EXCH_QUANT = 0.05
+SYMBOL = "ETH"
+class Trader:
 
-        tries = tries + 1
-        if tries > 50:
-            # cancel order
-            print(f"[{datetime.now().strftime('%H:%M:%S')}] cancelling...", end="")
-            r.orders.cancel_crypto_order(id)
-            order_info = r.orders.get_crypto_order_info(id)
-            while order_info["state"] != "canceled":
-                order_info = r.orders.get_crypto_order_info(id)
-                if order_info["state"] == "filled":
-                    print(
-                        f"cancel failed: bought [{datetime.now().strftime('%H:%M:%S')}] (Exec: ${round(float(order_info['average_price']), 2)})"
-                    )
-                    cleared = True
-                    return (True, float(order_info["average_price"]))
+  # This will serve as a list of ids for orders we're working on
+  buys: list
+  sells: list
 
-                time.sleep(50 / 1000)  # wait until the order is confirmed canceled
-            print(f"canceled [{datetime.now().strftime('%H:%M:%S')}]")
-            return (False, 0)
+  def __init__(self):
 
-        time.sleep(50 / 1000)  # sleep 50ms
+    self.buys = []
+    self.sells = []
 
+  # def checkForOpenOrders(self) -> Tuple(list, list):
+  #   open_buys = []
+  #   open_sells = []
+  #   all_opens_orders = r.get_all_open_crypto_orders()
+  #   for order in all_opens_orders:
+  #     if order['id'] in self.buys:
+  #       open_buys.append(order['id'])
+  #     if order['id'] in self.sells:
+  #       open_sells.append(order['id'])
+  #   print("Open Crypto Orders I've placed (buys/sells)", open_buys, open_sells)
+  #   return (open_buys, open_sells)
 
-def sellAndWait(r):
-    price_info = r.crypto.get_crypto_quote("ETH", info=None)
-    will_get = round(float(price_info["bid_price"]), 2)
-    # will_pay = round((float(price_info["bid_price"]) + float(price_info["mark_price"])) / 2, 2)
-    sell_info = r.orders.order_sell_crypto_limit("ETH", 0.05, will_get)
-    print(
-        f"[{datetime.now().strftime('%H:%M:%S')}] Selling (Limit: ${round(will_get, 2)})...",
-        end="",
-    )
-    cleared = False
-    if "id" not in sell_info:
-        print("NOT SURE WHAT IS GOING ON HERE, NO ID???")
-        print(sell_info)
-    id = sell_info["id"]
-    tries = 0
-    while not cleared:
-        order_info = r.orders.get_crypto_order_info(id)
-        if order_info["state"] == "filled":
-            print(
-                f"[{datetime.now().strftime('%H:%M:%S')}] sold (Exec: ${round(float(order_info['average_price']), 2)})"
-            )
-            cleared = True
-            return (True, float(order_info["average_price"]))
+  def buy(self, max_buy_price):
+      self.cancelOpenSells()
 
-        tries = tries + 1
-        if tries > 50:
-            # cancel order
-            print(f"[{datetime.now().strftime('%H:%M:%S')}] cancelling...", end="")
-            r.orders.cancel_crypto_order(id)
-            order_info = r.orders.get_crypto_order_info(id)
-            while order_info["state"] != "canceled":
-                order_info = r.orders.get_crypto_order_info(id)
-                if order_info["state"] == "filled":
-                    print(
-                        f"cancel failed: sold [{datetime.now().strftime('%H:%M:%S')}] (Exec: ${round(float(order_info['average_price']), 2)})"
-                    )
-                    cleared = True
-                    return (True, float(order_info["average_price"]))
-                time.sleep(50 / 1000)  # wait until the order is confirmed canceled
-            print(f"canceled [{datetime.now().strftime('%H:%M:%S')}].")
-            return (False, 0)
+      will_pay = round(max_buy_price, 2)
+      buy_info = r.orders.order_buy_crypto_limit(SYMBOL, EXCH_QUANT, will_pay)
+      trade = Trade('buy', buy_info["id"], will_pay, EXCH_QUANT)
+      self.buys.append(trade)
 
-        time.sleep(50 / 1000)  # sleep 50ms
+  def sell(self, min_sell_price):
+      self.cancelOpenSells()
+      
+      will_get = round(min_sell_price, 2)
+      buy_info = r.orders.order_sell_crypto_limit(SYMBOL, EXCH_QUANT, will_get)
+      trade = Trade('sell', buy_info["id"], will_get, EXCH_QUANT)
+      self.sells.append(trade)
+
+  def cancelOpenBuys(self):
+    open_buys = []
+    for buy in self.buys:
+      if buy.open:
+        open_buys.append(buy)
+    log.warning(f"Cancelling buy orders ({len(open_buys)})")
+    for trade in open_buys:
+      trade.cancel()
+
+  def cancelOpenSells(self):
+    open_sells = []
+    for trade in self.sells:
+      if trade.open:
+        open_sells.append(trade)
+    log.warning(f"Cancelling sell orders ({len(open_sells)})")
+    for trade in open_sells:
+      trade.cancel()
