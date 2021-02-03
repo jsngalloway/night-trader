4ce7bc900bc1ef5dc3e953386aa9bcb16d7c1dc5
jsngalloway <galloway.jesse@aurora.aero>
2021-02-03 10:24:28 -0500

    improve classic macd

--- night_trader/predictors/mac_daddy.py
+++ night_trader/predictors/mac_daddy.py
@@ -1,41 +1,84 @@
-import numpy as np
-from data_sourcer import DataSourcer
-from data_manager import DataManager
 import pandas as pd
+from datetime import datetime
+from predictors.lstm.lstm_data_manager import LstmDataManager
+import logging
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import MinMaxScaler
-import time
-from tqdm import tqdm
 
+log = logging.getLogger(__name__)
+
+# TODO REMOVE
+plt.style.use('seaborn-darkgrid')
+plt.ion()
+fig, axs = plt.subplots(2, figsize=(12.2, 7.5), sharex=True)
+plt.xticks(rotation=45)
+# Set common labels
+axs[0].set_xlabel('Time')
+axs[1].set_xlabel('Time')
+axs[0].set_ylabel('ETH in USD')
+plt.tight_layout()
+# ############################
 
 class MacDaddy:
 
     prices: pd.DataFrame = pd.DataFrame({"price": []})
-    dataManager: DataManager
+    dataManager: LstmDataManager
+    
+    macd_params = (20*4, 30*4, 11)
+    period_multiplier = 1  #175  # 25 or 111
 
-    def __init__(self, dataSourcer):
-        self.dataManager = DataManager(dataSourcer, 400)
-        for i in tqdm(range(0, 260)):
-            # while (len(self.dataManager.get()[1]) < 260):
-            i = len(self.dataManager.get()[1])
-            time.sleep(1)
-            # print('MAC Daddie Loading: gathered ' + str(len(self.dataManager.get()[1])) + '/260 prices')
+    def __init__(self, dataSourcer: LstmDataManager):
+        self.dataManager = dataSourcer
+        log.info("mac daddy predictor initialized")
+        log.info(f"Parameters: {self.macd_params}")
+        log.info(f"Period multiplier: {self.period_multiplier}")
 
-    def predict(self):
+    def predict(self, latest_price):
+        data = self.dataManager.getData(tail=5000, subsampling=1)
+        latest_time = str(data.time.iloc[-1])
+        latest_price = data.price.iloc[-1]
 
-        is_new, price_list = self.dataManager.get()
-        if not is_new:
-            return None
-        prices = pd.DataFrame(price_list)
+        # Smooth out data
+        data["price"] = data.price.ewm(span=6, adjust=True).mean()
 
-        exp1 = prices.ewm(span=117, adjust=False).mean()  # 12
-        exp2 = prices.ewm(span=254, adjust=False).mean()  # 26
+        # Calculate MACD and signal lines
+        exp1 = data[["price"]].ewm(span=self.macd_params[0] * self.period_multiplier, adjust=False).mean()
+        exp2 = data[["price"]].ewm(span=self.macd_params[1] * self.period_multiplier, adjust=False).mean()
         macd = exp1 - exp2
-        signal = macd.ewm(span=88, adjust=False).mean()  # 9
+        signal = macd.ewm(span=self.macd_params[2] * self.period_multiplier, adjust=False).mean()
 
-        if macd.iat[-1, 0] > signal.iat[-1, 0]:
+        data = data.set_index(pd.DatetimeIndex(pd.to_datetime(data['time'].values, utc=True))).tz_convert(tz='US/Eastern')
+        macd = macd.set_index(pd.DatetimeIndex(pd.to_datetime(data['time'].values, utc=True))).tz_convert(tz='US/Eastern')
+        signal = signal.set_index(pd.DatetimeIndex(pd.to_datetime(data['time'].values, utc=True))).tz_convert(tz='US/Eastern')
+
+        
+        # TODO Remove plotting
+        plt.rcParams['timezone'] = data.index.tz.zone
+        axs[0].clear()
+        axs[1].clear()
+        axs[0].plot(data.price.tail(1000), label='Price', linewidth=2)
+        axs[1].plot(macd.tail(1000), label='ETH MACD', color = 'red', linewidth=1)
+        axs[1].plot(signal.tail(1000), label='Signal Line', color='blue', linewidth=1)
+        axs[0].grid(b=True, which='both', axis='both')
+        plt.draw()
+        plt.pause(.001)
+        # --------------------------------------
+
+        log.debug(f"Using data ({data.time.iat[0]} {data.time.iat[-1]} : {len(data)})macd is at: {macd.price.iat[-1]} signal is at: {signal.price.iat[-1]}")
+
+        # using conventional logic...
+        if (macd.price.iat[-1] > signal.price.iat[-1]) and (
+            macd.price.iat[-2] < signal.price.iat[-2]
+        ):
+            log.info(f"Got BUY signal at [{latest_time}] ${latest_price:.2f}")
+            # wanna_buy = data.price.tail(5).mean() + ((data.price.tail(5).mean() - data.price.tail(5).min()) * 0.5)
+            # log.info(f"Will attempt to buy at: {wanna_buy}")
             return "buy"
-        elif macd.iat[-1, 0] < signal.iat[-1, 0]:
+        elif (macd.price.iat[-1] < signal.price.iat[-1]) and (
+            macd.price.iat[-2] > signal.price.iat[-2]
+        ):
+            log.info(f"Got SELL signal at [{latest_time}] ${latest_price:.2f}")
+            # wanna_sell = data.price.tail(5).mean() - ((data.price.tail(5).mean() - data.price.tail(5).max()) * 0.5)
+            # log.info(f"Will attempt to sell at: {wanna_sell}")
             return "sell"
         else:
             return None
