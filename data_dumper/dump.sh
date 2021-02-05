23e1693782c042771bedfd5ba851a543df7bc3f4
jsngalloway <galloway.jesse@aurora.aero>
2021-02-05 10:00:07 -0500

    Fixed printout

--- data_dumper/dump.sh
+++ data_dumper/dump.sh
@@ -1,8 +1,13 @@
 # eval "$(conda shell.bash hook)"
 # conda activate lstm
+echo "Pulling from git"
 git pull
+echo "Running Script"
 python3 /home/pi/night-trader/data_dumper/data_dumper.py /home/pi/night-trader/env /home/pi/night-trader/data
+echo "Commiting"
 git add data/*
-DATE=date +"%D %H:%M"
+DATE=$(date +"%D %H:%M")
+echo "Pushing Commit"
 git commit -m "New data from [$DATE]"
 git push
+echo "Done."
