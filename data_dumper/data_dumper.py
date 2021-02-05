82b1855e1044075fb1c2465c09e1f0541b81cda3
jsngalloway <galloway.jesse@aurora.aero>
2021-02-05 08:52:38 -0500

    dupe test 2

--- data_dumper/data_dumper.py
+++ data_dumper/data_dumper.py
@@ -35,14 +35,19 @@ def appendToFile(filePath: str, crypto_id: str):
     full_df = pd.DataFrame(raw_data)[
         ["begins_at", "open_price", "close_price", "high_price", "low_price", "volume"]
     ]
-    full_df.index = pd.DatetimeIndex(pd.to_datetime(full_df["begins_at"].values, utc=True))
+    # full_df.index = pd.DatetimeIndex(pd.to_datetime(full_df["begins_at"].values, utc=True))
 
     from_file = pd.read_csv(filePath)
-    from_file.index = pd.DatetimeIndex(pd.to_datetime(from_file["begins_at"].values, utc=True))
 
-    mask = ~full_df.index.isin(from_file)
-    result = full_df.loc[mask]
-    print(result)
+    dupes = full_df['begins_at'].isin(from_file['begins_at'])
+    print(f"Identified {len(dupes)} duplicates, dropping")
+    full_df.drop(full_df[dupes].index, inplace = True)
+    print(f"Now we have {len(full_df)} new rows to add")
+    # from_file.index = pd.DatetimeIndex(pd.to_datetime(from_file["begins_at"].values, utc=True))
+
+    # mask = ~full_df.index.isin(from_file)
+    # result = full_df.loc[mask]
+    # print(result)
 
     full_df.to_csv(
         filePath,
