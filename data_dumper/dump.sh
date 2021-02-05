be15a36cfd2d4d164be33ff22cd0c520069d66f1
jsngalloway <galloway.jesse@aurora.aero>
2021-02-05 09:51:47 -0500

    automated data upload

--- data_dumper/dump.sh
+++ data_dumper/dump.sh
@@ -1,3 +1,8 @@
 # eval "$(conda shell.bash hook)"
 # conda activate lstm
+git pull
 python3 /home/pi/night-trader/data_dumper/data_dumper.py /home/pi/night-trader/env /home/pi/night-trader/data
+git add data/*
+DATE=date +"%D %H:%M"
+git commit -m "New data from [$DATE]"
+git push
